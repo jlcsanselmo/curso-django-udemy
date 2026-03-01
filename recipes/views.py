8a020a9bf8abd96a7ac920db4db3b2f6c13db187
jlcsanselmo <jlcsanselmo@gmail.com>
2026-03-01 19:18:32 -0300

    iniciando o site de receitas veg

--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,6 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-def sobre(request):
-    return HttpResponse("sobre")
-
-def contato(request):
-    return HttpResponse("contato")
+    return render(request, 'recipes/home.html', context={
+        'name': 'João Lucas',
+    })
