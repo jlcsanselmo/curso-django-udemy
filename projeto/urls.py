755b1495b4c5cc9afc299b9e63af7477b75b917d
jlcsanselmo <jlcsanselmo@gmail.com>
2026-03-01 14:51:49 -0300

    criando app e ajustando urls e views

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+from recipes.views import home, sobre, contato
 
-
-def my_view(request):
-    return HttpResponse("Olá, mundo!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view )
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato),
 ]
