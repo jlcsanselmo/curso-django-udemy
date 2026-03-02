84c15111b494b25878de280ddff1ae9270847669
jlcsanselmo <jlcsanselmo@gmail.com>
2026-03-01 21:31:25 -0300

    adicionando style dentro do home.html

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name': 'João Lucas',
-    })
+    return render(request, 'pages/home.html',)
